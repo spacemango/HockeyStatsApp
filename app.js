181e8c4e91ee7368c9a9dd594e4caa338a416bc1
spacemango <zankubongo@gmail.com>
2022-08-11 01:04:11 -0700

    Bug fixes

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ import * as tables from './assets/js/tables.js';
 const lastModified = document.querySelector("#last-modified");
 const navItem = document.querySelector('#nav-item');
 
-// Date for last modified
-// TODO: Need a fix for dynamically generated page
-const getDate = function () {
-   const date = new Date();
-   lastModified.innerHTML = `Last updated: ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-};
-
 // Event listeners
 // Team Stats
 const teamsListener = function () {
